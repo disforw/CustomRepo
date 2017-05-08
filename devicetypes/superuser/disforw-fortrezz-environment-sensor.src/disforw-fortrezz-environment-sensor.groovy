6fbea8442fd5dc23038b9aca8115b6be59a461e8
Ben Abrams <smartthings@benzona.net>
2017-05-08 15:57:21 -0400

    Changed temp color scale

--- devicetypes/superuser/disforw-fortrezz-environment-sensor.src/disforw-fortrezz-environment-sensor.groovy
+++ devicetypes/superuser/disforw-fortrezz-environment-sensor.src/disforw-fortrezz-environment-sensor.groovy
@@ -55,13 +55,13 @@ metadata {
         valueTile("temperature", "device.temperature", width: 3, height: 2) {
 			state("temperature", label:'${currentValue}°', unit:"F",
 				backgroundColors:[
-					[value: 31, color: "#153591"],
-					[value: 44, color: "#1e9cbb"],
-					[value: 59, color: "#90d2a7"],
-					[value: 74, color: "#44b621"],
-					[value: 84, color: "#f1d801"],
-					[value: 95, color: "#d04e00"],
-					[value: 96, color: "#bc2323"]
+					[value: 40, color: "#153591"],
+					[value: 50, color: "#1e9cbb"],
+					[value: 60, color: "#90d2a7"],
+					[value: 70, color: "#44b621"],
+					[value: 75, color: "#f1d801"],
+					[value: 80, color: "#d04e00"],
+					[value: 85, color: "#bc2323"]
 				]
 			)
 		}
